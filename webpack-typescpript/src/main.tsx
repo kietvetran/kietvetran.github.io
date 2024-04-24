ccc8103c664c2ebb2306c01e77fb509eef6e3b90
Kiet Ve Tran <kiet.ve.tran@eika.no>
2024-04-24 07:08:03 +0200

    aded: webpack-typescript

---
+++ webpack-typescpript/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import Application from './components/Application';
+
+// Application to Render
+const app = <Application />;
+
+// Render application in DOM
+createRoot(document.getElementById('app')).render(app);
