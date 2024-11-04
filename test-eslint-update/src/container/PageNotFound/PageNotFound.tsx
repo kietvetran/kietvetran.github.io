3979694c5ff7e3b68dd26cacc91f887b25b135d2
Kiet Ve Tran <kiet.ve.tran@entur.org>
2024-11-04 11:42:02 +0100

    Remove containter files.

--- test-eslint-update/src/container/PageNotFound/PageNotFound.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './PageNotFound.scss';
-
-export default function PageNotFound() {
-  return <div className="page-not-found-wrapper">Page not found</div>;
-}
--- test-eslint-update/src/container/Test/Test.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Test.scss';
-
-export default function Test() {
-  return <div className="test-wrapper">Test</div>;
-}
