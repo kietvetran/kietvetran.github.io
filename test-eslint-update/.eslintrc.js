36538a6f78c187a9e86f16f281efff252ab77f9e
Kiet Ve Tran <kiet.ve.tran@entur.org>
2024-11-04 11:43:26 +0100

    test - step 2.

--- test-eslint-update/.eslintrc.js
+++ test-eslint-update/.eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   extends: ['universe/native', 'universe/shared/typescript-analysis', 'plugin:@tanstack/eslint-plugin-query/recommended'],
   rules: {
-    '@typescript-eslint/no-unused-vars': ['warn', { caughtErrors: 'all', caughtErrorsIgnorePattern: '^(e|error)$' }],
+    'no-continue': 'off'
   },
   overrides: [
     {
