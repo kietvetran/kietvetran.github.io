3d4ea72a3edccf7e50f03c6da4112fa8278eb962
Kiet Ve Tran <kiet.ve.tran@entur.org>
2025-03-02 11:12:28 +0100

    update lib.

--- eika/spareadmin/src/hooks/usePrevious.ts
+++ eika/spareadmin/src/hooks/usePrevious.ts
@@ -1,11 +1,19 @@
 import { useEffect, useRef } from 'react';
 
-const usePrevious = (value: any): any => {
- 	const reference = useRef<any>(null);
- 	useEffect(() => {
-   		reference.current = value;
-  	}, [value]);
-  	return reference.current;
+export default function usePrevious<T>(value: T): T | undefined {
+  const previousRef = useRef<T | undefined>(undefined);
+
+  useEffect( () => {
+    previousRef.current = value;
+  }, [value, previousRef]);
+
+  return previousRef.current;
 };
 
-export default usePrevious;
+/*
+export const sortItemSize = <T>(aItem: T & {size?: number}, bItem: T & {size?: number}): number => {
+  const a = aItem.size ?? 0;
+  const b = bItem.size ?? 0;
+  return a - b;
+};
+*/
--- test-eslint-update/README.md
+++
@@ -1,2 +0,0 @@
-# kietvetran@github.io
-
