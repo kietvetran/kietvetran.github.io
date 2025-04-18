7b59dec6a50dd2f4df4159136894a32219f9fce3
Kiet Ve Tran <kiet.ve.tran@entur.org>
2025-04-18 15:33:03 +0200

    kvt-unitiy-test: Ep7 - move shape.

--- kvt-ventures/unity-test/codeplanstudio-block-adventure/Assets/Scripts/Shape/Shape.cs
+++ kvt-ventures/unity-test/codeplanstudio-block-adventure/Assets/Scripts/Shape/Shape.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
-public class Shape : MonoBehaviour {
+public class Shape : MonoBehaviour, IPointerClickHandler, IPointerUpHandler, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler {
   public GameObject squareShapeImage;
+  public Vector3 shapeSelectedScale;
+  public Vector2 offset = new Vector2(0f, 700f);
 
   [HideInInspector]
   public ShapeData CurrentShapeData;
 
   private List<GameObject> _currentShape = new List<GameObject>();
+  private Vector3 _shapeStartScale;
+  private RectTransform _transform;
+  private bool _shapeDraggable = true;
+  private Canvas _canvas;
 
   void Start() {
     // RequestNewShape(CurrentShapeData);
+  }
+
+  public void Awake() {
+    _shapeStartScale = this.GetComponent<RectTransform>().localScale;
+    _transform = this.GetComponent<RectTransform>();
+    _canvas = GetComponentInParent<Canvas>();
+    _shapeDraggable = true;
+
   }
 
   public void RequestNewShape( ShapeData shapeData ) {
@@ -134,5 +149,42 @@ public class Shape : MonoBehaviour {
   private float GetMiddle(int count) {
     int leftOver = count % 2;
     return (count - leftOver) / 2;
+  }
+
+  /**
+   * Required POINT handler that is inherited.  
+   */
+
+  public void OnPointerClick( PointerEventData eventData ) {    
+  }
+
+  public void OnPointerUp( PointerEventData eventData ) {    
+  }
+
+  public void OnBeginDrag( PointerEventData eventData ) {
+    this.GetComponent<RectTransform>().localScale = shapeSelectedScale;
+  }
+
+  public void OnDrag( PointerEventData eventData ) {
+    _transform.anchorMin = new Vector2(0, 0); 
+    _transform.anchorMax = new Vector2(0, 0); 
+    _transform.pivot = new Vector2(0, 0); 
+
+    Vector2 position;
+    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+      _canvas.transform as RectTransform,
+      eventData.position,
+      Camera.main,
+      out position
+    );
+
+    _transform.localPosition = position + offset;
+  }
+
+  public void OnEndDrag( PointerEventData eventData ) { 
+    this.GetComponent<RectTransform>().localScale = _shapeStartScale;
+  }
+
+  public void OnPointerDown( PointerEventData eventData ) {    
   }
 }
